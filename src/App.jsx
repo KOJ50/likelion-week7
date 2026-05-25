cfed8d4cd08d8c9f646da9ae0693b4b7c702cf1d
juii <dlwngml480@naver.com>
2026-05-25 14:21:24 +0900

    [FEAT] CartList 컴포넌트 props 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import CartList from "./components/CartList";
 function App() {
   return (
     <div>
-      <CartList />
+      <CartList
+        count={2}
+        restaurantName="왕꼬치"
+        firstMenu="닭꼬치 세트 (5ea)"
+        firstPrice="12,000"
+        secondMenu="떡꼬치"
+        secondPrice="5,000"
+      />
     </div>
   );
 }
--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -2,19 +2,26 @@ import IconPlus from "../assets/icons/icon_plus.svg?react";
 import IconClose from "../assets/icons/icon_close.svg?react";
 import IconMinus from "../assets/icons/icon_minus.svg?react";
 
-const CartList = ({ count = 2 }) => {
+const CartList = ({
+  count,
+  mainName,
+  firstMenu,
+  firstPrice,
+  secondMenu,
+  secondPrice,
+}) => {
   return (
     <div className="w-[247px] ph:w-[561px] rounded-[var(--radius-small)] overflow-hidden bg-[var(--color-gray-0)]">
       <div className="bg-[var(--color-yellow-primary)] px-5 py-3 text-body-bold">
-        왕꼬치
+        {mainName}
       </div>
 
       <div className="flex flex-col gap-[10px] ph:p-0">
         <div className="bg-[var(--color-gray-0)] rounded-[var(--radius-small)] ph:rounded-none flex flex-col ph:flex-row ph:items-center ph:justify-between px-5 py-3 ">
           <div className="flex flex-col gap-2">
-            <div className="text-subtitle">닭꼬치 세트 (5ea)</div>
+            <div className="text-subtitle">{firstMenu}</div>
             <div className="text-subtitle text-[var(--color-red-primary)]">
-              12,000
+              {firstPrice}
             </div>
           </div>
           <div className="flex items-center gap-8 mt-3 ph:mt-0">
@@ -34,9 +41,9 @@ const CartList = ({ count = 2 }) => {
         {count === 2 && (
           <div className="bg-[var(--color-gray-0)] rounded-[var(--radius-small)] ph:rounded-none flex flex-col ph:flex-row ph:items-center ph:justify-between px-5 py-3 border-t border-[var(--color-gray-2)]">
             <div className="flex flex-col gap-2">
-              <div className="text-subtitle">떡꼬치</div>
+              <div className="text-subtitle">{secondMenu}</div>
               <div className="text-subtitle text-[var(--color-red-primary)]">
-                5,000
+                {secondPrice}
               </div>
             </div>
             <div className="flex items-center gap-8 mt-3 ph:mt-0">
