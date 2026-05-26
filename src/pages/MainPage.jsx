7cbed19ae45ee41589b0cc13d1d6a436baa47860
juii <dlwngml480@naver.com>
2026-05-26 21:43:26 +0900

    [FEAT] 메인페이지 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import FoodCard from "../components/FoodCard.jsx";
+import foodData from "../data/mockFoodCard.js";
+import OptionTag from "../components/OptionTag.jsx";
+
+const categories = ["전체", "분식", "기타"];
+
 function MainPage() {
-  return <main>메인 페이지</main>;
+  const [activeCategory, setActiveCategory] = useState("전체");
+
+  return (
+    <main className="w-[1200px] flex flex-col gap-[72px]">
+      <div className="flex gap-[24px] mb-6">
+        {categories.map((cat) => (
+          <div key={cat} onClick={() => setActiveCategory(cat)}>
+            <OptionTag text={cat} isSelected={activeCategory === cat} />
+          </div>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+        {foodData.map((food) => (
+          <FoodCard
+            key={food.id}
+            image={food.image}
+            name={food.name}
+            rate={food.rate}
+            caption={food.caption}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default MainPage;
