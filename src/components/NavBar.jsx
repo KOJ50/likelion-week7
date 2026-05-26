a5f29cbebdb8ad1b1b3b2ed61c94696f40b0e9fd
juii <dlwngml480@naver.com>
2026-05-26 22:38:30 +0900

    [FEAT] Navbar 구현

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import Cart from "./Cart.jsx";
+
+const NavBar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <header className="w-full h-[83px] bg-[#F0485F] flex items-center justify-between px-3 min-[403px]:px-10 py-[20px] select-none">
+      <h1
+        className="text-white text-lg min-[403px]:text-2xl font-bold cursor-pointer whitespace-nowrap"
+        onClick={() => navigate("/")}
+      >
+        어쩌구 저쩌구
+      </h1>
+
+      {/* 오른쪽 메뉴 영역 */}
+      <div className="flex items-center gap-2 min-[403px]:gap-6 text-white min-w-max">
+        <Cart count={1} />
+        <button className="text-xs min-[403px]:text-sm hover:bg-white/30 rounded-sm cursor-pointer px-2 min-[403px]:px-3 py-1.5 whitespace-nowrap">
+          로그아웃
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import FoodCard from "../components/FoodCard.jsx";
 import foodData from "../data/mockFoodCard.js";
 import OptionTag from "../components/OptionTag.jsx";
+import NavBar from "../components/Navbar.jsx";
 
 const categories = ["전체", "분식", "기타"];
 
@@ -9,26 +10,29 @@ function MainPage() {
   const [activeCategory, setActiveCategory] = useState("전체");
 
   return (
-    <main className="w-[1200px] flex flex-col gap-[72px]">
-      <div className="flex gap-[24px] mb-6">
-        {categories.map((cat) => (
-          <div key={cat} onClick={() => setActiveCategory(cat)}>
-            <OptionTag text={cat} isSelected={activeCategory === cat} />
-          </div>
-        ))}
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-        {foodData.map((food) => (
-          <FoodCard
-            key={food.id}
-            image={food.image}
-            name={food.name}
-            rate={food.rate}
-            caption={food.caption}
-          />
-        ))}
-      </div>
-    </main>
+    <div className="w-full min-h-screen bg-gray-50 flex flex-col items-center">
+      <NavBar />
+      <main className="w-full max-w-[1200px] px-4 flex flex-col pt-[90px] pb-[100px]">
+        <div className="flex gap-[24px] mb-6">
+          {categories.map((cat) => (
+            <div key={cat} onClick={() => setActiveCategory(cat)}>
+              <OptionTag text={cat} isSelected={activeCategory === cat} />
+            </div>
+          ))}
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mt-[72px]">
+          {foodData.map((food) => (
+            <FoodCard
+              key={food.id}
+              image={food.image}
+              name={food.name}
+              rate={food.rate}
+              caption={food.caption}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
   );
 }
 
