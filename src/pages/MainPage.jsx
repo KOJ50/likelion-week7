e970e20d4f51eb41750523bfe6d58b1aa189dbd2
juii <dlwngml480@naver.com>
2026-05-26 23:03:52 +0900

    [FEAT] 음식 눌렀을때 모달 뜨는거 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,11 +3,13 @@ import FoodCard from "../components/FoodCard.jsx";
 import foodData from "../data/mockFoodCard.js";
 import OptionTag from "../components/OptionTag.jsx";
 import NavBar from "../components/Navbar.jsx";
+import ModalMenu from "../components/ModalMenu.jsx";
 
 const categories = ["전체", "분식", "기타"];
 
 function MainPage() {
   const [activeCategory, setActiveCategory] = useState("전체");
+  const [selectedMenu, setSelectedMenu] = useState(null);
 
   return (
     <div className="w-full min-h-screen bg-gray-50 flex flex-col items-center">
@@ -20,18 +22,31 @@ function MainPage() {
             </div>
           ))}
         </div>
+
+        {/* 음식 카드 리스트 */}
         <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mt-[72px]">
           {foodData.map((food) => (
-            <FoodCard
-              key={food.id}
-              image={food.image}
-              name={food.name}
-              rate={food.rate}
-              caption={food.caption}
-            />
+            <div key={food.id} onClick={() => setSelectedMenu(food)}>
+              <FoodCard
+                key={food.id}
+                image={food.image}
+                name={food.name}
+                rate={food.rate}
+                caption={food.caption}
+              />
+            </div>
           ))}
         </div>
       </main>
+      {/* 모달 */}
+      {selectedMenu && (
+        <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+          <ModalMenu
+            menu={selectedMenu}
+            onClose={() => setSelectedMenu(null)}
+          />
+        </div>
+      )}
     </div>
   );
 }
